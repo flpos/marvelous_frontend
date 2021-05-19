1b904e4663ff1e5f51553157c2379545aec815fd
Felipe Oliveira <fsousa@tasken.com>
2021-05-19 01:59:46 -0300

    feature: styled-components e estilos globais.

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
+import { GlobalStyles } from './globalStyles';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyles />
       <App />
     </Provider>
   </React.StrictMode>,
