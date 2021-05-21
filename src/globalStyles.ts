b057bc4328b721a3d98a3b8a8a6658529c984074
Felipe Oliveira <fsousa@tasken.com>
2021-05-21 10:53:17 -0300

    fix: Atualizando Estilos globais

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -3,7 +3,10 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    background-color: #333;
+    font-family: Roboto;
+    background-color: #101010;
+    * {
+      box-sizing: border-box;
+    }
   }
 `;
