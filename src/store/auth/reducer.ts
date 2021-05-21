593fa74074850503e4c86efd20a69c42a93e16f2
Felipe Oliveira <fsousa@tasken.com>
2021-05-21 10:53:33 -0300

    fix: Login e persistência de token.

--- src/store/auth/reducer.ts
+++ src/store/auth/reducer.ts
@@ -1,12 +1,16 @@
 import { Reducer } from 'redux';
 import { AuthActions } from './actions';
 
-const INITIAL_STATE = { token: '' };
+const INITIAL_STATE = { token: localStorage.getItem('token') ?? '' };
 
 const authReducer: Reducer<AuthReducer> = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case AuthActions.LOGIN:
+      localStorage.setItem('token', action.payload);
       return { ...state, token: action.payload };
+    case AuthActions.LOGOUT:
+      localStorage.removeItem('token');
+      return { token: '' };
     default:
       return state;
   }
