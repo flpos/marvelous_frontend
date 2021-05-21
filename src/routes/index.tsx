ceae33621acac881d5531695e1d3e6f8536b7ceb
Felipe Oliveira <fsousa@tasken.com>
2021-05-21 11:00:26 -0300

    Definição das rotas.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+import CharacterPage from '../pages/CharacterPage';
+import CharactesSearch from '../pages/CharactesSearch';
+import ComicPage from '../pages/ComicPage';
+import ComicsSearch from '../pages/ComicsSearch/ComicsSearch';
+import Favorites from '../pages/Favorites';
 import Login from '../pages/Login';
-import Route from './Route';
+import SignUp from '../pages/SignUp';
+import UserPanel from '../pages/UserPanel';
+import { RootState } from '../store/DefaultRootState';
 
 const Routes = () => {
-  return (
-    <Router>
-      <Route path='/login' exact component={Login} />
-      <Route isPrivate path='/' exact component={() => <div>Root</div>} />
-    </Router>
+  const token = useSelector((state: RootState) => state.auth.token);
+  return token ? (
+    <>
+      <Router>
+        <NavBar />
+        <Route path='/' exact component={Favorites} />
+        <Route path='/panel' exact component={UserPanel} />
+        <Route path='/comics-search' exact component={ComicsSearch} />
+        <Route path='/characters-search' exact component={CharactesSearch} />
+        <Route path='/characters/:id' exact component={CharacterPage} />
+        <Route path='/comics/:id' exact component={ComicPage} />
+        <Route path='*' component={() => <Redirect to='/' />} />
+      </Router>
+    </>
+  ) : (
+    <>
+      <Router>
+        <Route path='/login' exact component={Login} />
+        <Route path='/signUp' exact component={SignUp} />
+        <Route path='*' component={() => <Redirect to='/Login' />} />
+      </Router>
+    </>
   );
 };
 
